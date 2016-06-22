f4203237bfdc776a80a1be55cdda27e1490fbf0b
Evan <Evan@catskindemacbook-air-2.local>
2016-06-22 10:05:44 +0800

    Initial Commit

---
+++ testDog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testDog
+//
+//  Created by Evan on 2016/6/22.
+//  Copyright © 2016年 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
